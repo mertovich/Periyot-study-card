c3859832c7c50b68a31a044682ab4f4bdeb4a675
mertovich <88425310+mertovich@users.noreply.github.com>
2022-02-10 12:47:16 +0300

    add(ListTopBar)

---
+++ mds_study_card/components/ListTopBar.js
@@ -0,0 +1,55 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  Pressable,
+} from 'react-native';
+import React from 'react';
+
+const ListTopBar = () => {
+  return (
+    <View>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Search..."
+          placeholderTextColor={'white'}
+        />
+        <Pressable style={styles.addButton}>
+          <Image
+            style={styles.image}
+            source={require('../assets/Icon-ionic-add-circle-outline.png')}
+          />
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: '5%',
+    justifyContent: 'space-evenly',
+  },
+  textInput: {
+    backgroundColor: '#707070',
+    width: '70%',
+    padding: '3%',
+    borderRadius: 10,
+    fontSize: 18,
+    color: 'white',
+  },
+  image: {
+    height: 40,
+    width: 40,
+  },
+  addButton: {
+    marginTop: '1.5%',
+  }
+});
+
+export default ListTopBar;
