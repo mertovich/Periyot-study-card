1dba330b9972c50c90f8f30949e4ab1eb9363c14
Periyot <mdsabinov@gmail.com>
2022-02-11 14:16:05 +0300

    add(ListAddPage)

--- mds_study_card/Pages/ListAdd.js
+++ mds_study_card/Pages/ListAdd.js
@@ -1,10 +1,26 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet, TextInput, Pressable, Text} from 'react-native';
 import React from 'react';
 
 const ListAdd = () => {
   return (
     <View style={styles.container}>
-      <Text>ListAdd</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Word"
+          placeholderTextColor={'white'}
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Meaning of the word"
+          placeholderTextColor={'white'}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Pressable style={styles.saveButton}>
+          <Text style={styles.saveButtonText}>Save word</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -12,6 +28,33 @@ const ListAdd = () => {
 const styles = StyleSheet.create({
   container: {
     height: '85%',
+  },
+  textInput: {
+    backgroundColor: '#707070',
+    width: '90%',
+    padding: '3%',
+    borderRadius: 10,
+    fontSize: 18,
+    color: 'white',
+    marginTop: '10%',
+  },
+  inputContainer: {
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    marginTop: '10%',
+  },
+  saveButton: {
+    backgroundColor: '#AC6935',
+    width: '80%',
+    alignItems: 'center',
+    padding: '1%',
+    borderRadius: 15,
+  },
+  saveButtonText: {
+    color: 'white',
+    fontSize: 24,
   },
 });
 
