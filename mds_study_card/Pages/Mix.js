13c6d85e326a3d17b48b33421ec7e3217c603ccd
Periyot <mdsabinov@gmail.com>
2022-02-11 13:42:41 +0300

    fix(Navigation): Navigation render error

---
+++ mds_study_card/Pages/Mix.js
@@ -0,0 +1,18 @@
+import {View, Text, StyleSheet} from 'react-native';;
+import React from 'react';
+
+const Mix = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Mix</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: '85%',
+  },
+});
+
+export default Mix;
